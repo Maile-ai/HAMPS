c89f5dc1fd31ac50c1b69f7333ddb6399cc07cd4
Mokete Maile <maile.s.mokete@gmail.com>
2025-12-06 16:19:40 +0200

    Fixed INSTALLED_APPS configuration error

--- hamp_project/settings.py
+++ hamp_project/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
-    'django_rest_framework',
+
+    'rest_framework',
     'rest_framework.authtoken',
+    'users',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -123,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
