b2fac08e01e3f232ab0bf2a90e3d80b0d3016370
Mokete Maile <maile.s.mokete@gmail.com>
2025-12-13 13:18:56 +0200

    Configured Django admin for devices and restriction rules

--- devices/admin.py
+++ devices/admin.py
@@ -1,6 +1,59 @@
 from django.contrib import admin
-from .models import Device, RestrictionRule, AccessLog
+from .models import Device, RestrictionRule
 
-admin.site.register(Device)
-admin.site.register(RestrictionRule)
-admin.site.register(AccessLog)
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = (
+        "hostname",
+        "ip_address",
+        "mac_address",
+        "device_type",
+        "connection_type",
+        "group",
+        "owner",
+        "last_seen",
+    )
+
+    list_filter = (
+        "group",
+        "connection_type",
+        "device_type",
+    )
+
+    search_fields = (
+        "hostname",
+        "ip_address",
+        "mac_address",
+        "owner__username",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "last_seen",
+    )
+
+    ordering = ("-last_seen",)
+
+
+@admin.register(RestrictionRule)
+class RestrictionRuleAdmin(admin.ModelAdmin):
+    list_display = (
+        "group",
+        "active",
+        "block_internet",
+        "schedule_start",
+        "schedule_end",
+        "created_at",
+    )
+
+    list_filter = (
+        "active",
+        "group",
+    )
+
+    readonly_fields = (
+        "created_at",
+    )
+
+    ordering = ("group",)
