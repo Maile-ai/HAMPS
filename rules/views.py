2d365b415ce30576f4705b6b38378d3650aadaf8
Mokete Maile <maile.s.mokete@gmail.com>
2025-12-14 19:28:23 +0200

    Added serializers and urls for rules app and wired endpoints

--- rules/views.py
+++ rules/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Rule
+from .serializers import RuleSerializer
 
-# Create your views here.
+
+class RuleListCreateView(generics.ListCreateAPIView):
+    serializer_class = RuleSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Rule.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class RuleDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = RuleSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Rule.objects.filter(owner=self.request.user)
